66d87efc6cb5a2d79d3d4183d3bc7d584f7908ff
Mert Sakızcı <mertskzc@gmail.com>
2019-03-15 14:07:04 +0300

    First commit

---
+++ ECGTest/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ECGTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(695, 662);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "FİLTRESİZ";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(799, 12);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(695, 662);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "FİLTRELİ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(555, 643);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "FİLTRESİZ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(1362, 643);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "FİLTRELİ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1506, 688);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ECGTest/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ECGTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.MouseWheel += chart1_MouseWheel;
+            chart2.MouseWheel += chart1_MouseWheel;
+        }
+
+        int[] test1 = data.data1;
+        int[] test2 = data.data2;
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.DataBindY(test2);
+            
+            double[] filtrelenecekData = new double[test2.Length];
+            for (int i = 0; i < test2.Length; i++)
+            {
+                filtrelenecekData[i] = Convert.ToDouble(test2[i]);
+            }
+
+            double[] filtreliData = Butterworth(filtrelenecekData, 512, 0.002);//BUTTERWORTH LOWPASS CALL
+
+            int[] filtreliTest = new int[filtreliData.Length];
+            for (int i = 0; i < filtreliData.Length; i++)
+            {
+                filtreliTest[i] = Convert.ToInt32(filtreliData[i]);
+            }
+
+            chart2.Series[0].Points.DataBindY(filtreliTest);
+
+        }
+        private void chart1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            var chart = (System.Windows.Forms.DataVisualization.Charting.Chart)sender;
+            var xAxis = chart.ChartAreas[0].AxisX;
+            var yAxis = chart.ChartAreas[0].AxisY;
+
+            try
+            {
+                if (e.Delta < 0) // Scrolled down.
+                {
+                    xAxis.ScaleView.ZoomReset();
+                    yAxis.ScaleView.ZoomReset();
+                }
+                else if (e.Delta > 0) // Scrolled up.
+                {
+                    var xMin = xAxis.ScaleView.ViewMinimum;
+                    var xMax = xAxis.ScaleView.ViewMaximum;
+                    var yMin = yAxis.ScaleView.ViewMinimum;
+                    var yMax = yAxis.ScaleView.ViewMaximum;
+
+                    var posXStart = xAxis.PixelPositionToValue(e.Location.X) - (xMax - xMin) / 4;
+                    var posXFinish = xAxis.PixelPositionToValue(e.Location.X) + (xMax - xMin) / 4;
+                    var posYStart = yAxis.PixelPositionToValue(e.Location.Y) - (yMax - yMin) / 4;
+                    var posYFinish = yAxis.PixelPositionToValue(e.Location.Y) + (yMax - yMin) / 4;
+
+                    xAxis.ScaleView.Zoom(posXStart, posXFinish);
+                    yAxis.ScaleView.Zoom(posYStart, posYFinish);
+                }
+            }
+            catch { }
+        }
+        public static double[] Butterworth(double[] indata, double deltaTimeinsec, double CutOff)
+        {
+            if (indata == null) return null;
+            if (CutOff == 0) return indata;
+
+            double Samplingrate = 1 / deltaTimeinsec;
+            long dF2 = indata.Length - 1;        // The data range is set with dF2
+            double[] Dat2 = new double[dF2 + 4]; // Array with 4 extra points front and back
+            double[] data = indata; // Ptr., changes passed data
+            
+            for (long r = 0; r < dF2; r++)
+            {
+                Dat2[2 + r] = indata[r];
+            }
+            Dat2[1] = Dat2[0] = indata[0];
+            Dat2[dF2 + 3] = Dat2[dF2 + 2] = indata[dF2];
+            
+            const double pi = 3.14159265358979;
+            double wc = Math.Tan(CutOff * pi / Samplingrate);
+            double k1 = 1.414213562 * wc; // Sqrt(2) * wc
+            double k2 = wc * wc;
+            double a = k2 / (1 + k1 + k2);
+            double b = 2 * a;
+            double c = a;
+            double k3 = b / k2;
+            double d = -2 * a + k3;
+            double e = 1 - (2 * a) - k3;
+            
+            double[] DatYt = new double[dF2 + 4];
+            DatYt[1] = DatYt[0] = indata[0];
+            for (long s = 2; s < dF2 + 2; s++)
+            {
+                DatYt[s] = a * Dat2[s] + b * Dat2[s - 1] + c * Dat2[s - 2]
+                           + d * DatYt[s - 1] + e * DatYt[s - 2];
+            }
+            DatYt[dF2 + 3] = DatYt[dF2 + 2] = DatYt[dF2 + 1];
+            
+            double[] DatZt = new double[dF2 + 2];
+            DatZt[dF2] = DatYt[dF2 + 2];
+            DatZt[dF2 + 1] = DatYt[dF2 + 3];
+            for (long t = -dF2 + 1; t <= 0; t++)
+            {
+                DatZt[-t] = a * DatYt[-t + 2] + b * DatYt[-t + 3] + c * DatYt[-t + 4]
+                            + d * DatZt[-t + 1] + e * DatZt[-t + 2];
+            }
+            
+            for (long p = 0; p < dF2; p++)
+            {
+                data[p] = DatZt[p];
+            }
+
+            return data;
+        }
+    }
+}
